4c0dbb299aa4f001656fb59f4cec95ad7fd3a9d8
Jackwelyne <36117873+SarcaCircus@users.noreply.github.com>
2018-02-20 16:07:38 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,26 @@
 #include <stdlib.h>
-
 void insert(const int* from, int* to, int value, size_t location, size_t toLength) {
-	int fromIndex = 0;    
-	for (int toIndex = 0; toIndex < toLength; ++toIndex) { 
-		if (toIndex != location) { 
+	int fromIndex = 0;
+	for (int toIndex = 0; toIndex < toLength; ++toIndex) {
+		if (toIndex != location) {
 			to[toIndex] = from[fromIndex];
-			++fromIndex; } 
-		else { to[toIndex] = value; }
+			++fromIndex; }
+		else {
+			to[toIndex] = value;
+		}
 	}
 }
 void remove(const int* from, int* to, size_t location, size_t fromLength) {
-	int fromIndex = 0;
-	int remove[] = 0;
-	
-
-	for (int Index = 0; Index<fromLength; ++Index) {
-		for (int j = 0; j < sizeof(to); ++j) {
-			if (Index == location) {
-				to[j] = from[fromIndex];
-				from[Index] = remove;
-			}
-			else ++fromIndex;
+	int blank = 0;
+	for (int remove = 0; remove < fromLength; ++remove) {
+		
+		if (remove == location) {
+			insert(from, to, from[remove], (size_t)remove, sizeof(to));
+			insert(to, from, blank, from[remove], fromLength);
+			
 		}
 	}
-	
+
 }
 int main() {
 	int arr[5] = { 1, 2, 3, 4, 5 };
@@ -32,3 +29,4 @@ int main() {
 	int arr3[5];
 	remove(arr2, arr3, 3, 6);
 }
+
